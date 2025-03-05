cfe28cbc97d42e617a559670f67af58c33a2004f
Eric-Philippe <ericphlpp@gmail.com>
2025-03-05 20:40:20 +0100

    Remove API key from the input path

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/api", routes);
+app.use("/", routes);
 
 app.use(errorHandler);
 
